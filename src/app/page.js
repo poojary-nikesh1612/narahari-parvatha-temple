77700fd0c85600fa38538a567215d0ae1a4b67ce
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-12-14 10:34:00 +0530

    changing site title

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,16 @@ import FestivalsComponent from "@/components/FestivalsComponent";
 import LocationComponent from "@/components/LocationComponent";
 
 export const metadata = {
-  title: "Narahari Parvatha - The Kailasa of the Earth",
+  title: "Narahari Parvatha Sadashiva Temple | Narahari Parvatha",
   description:
     "Visit Narahari Parvatha in Bantwal. Climb 333 steps to reach Lord Sadashiva. A historic spot where Arjuna and Krishna performed penance.",
   keywords: [
+    "Narahari Parvatha",
+    "Sadashiva Temple Bantwal",
+    "Kailasa of the Earth",
+    "Mangalore Pilgrimage Centers",
+    "Shiva Temples Dakshina Kannada",
+    "Hilltop Temples Karnataka",
     "Narahari Hill",
     "Bantwal Tourism",
     "333 Steps Temple",
